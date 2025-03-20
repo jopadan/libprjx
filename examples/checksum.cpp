0065af819b4ddd1dab56eb0be0513981d2259135
Jon Daniel <joneqdaniel@gmail.com>
2025-03-20 18:34:02 +0100

    update prjxsum

--- examples/checksum.cpp
+++ examples/checksum.cpp
@@ -10,14 +10,13 @@ int main(int argc, char** argv)
 		printf("Usage: %s [FILE]...\nCalculate Forsaken ProjectX style checksums for FILEs (the current directory by default).\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	i64 sum[2] = { -1, -1 };
 	for(int i = 1; i < argc && std::filesystem::exists(argv[i]); i++)
 	{
+		i64 sum[2] = { -1, -1 };
 		/* print checksum of file */
 		sum[0] = checksum::verify(argv[i]);
 
 		u64 len  = std::filesystem::file_size(argv[i]);
-		u64 read = 0;
 
 		if(len > 0)
 		{
@@ -30,8 +29,9 @@ int main(int argc, char** argv)
 			if(len == read)
 				sum[1] = checksum::verify(buf, len);
 			free(buf);
+			buf = NULL;
 			if(sum[0] != sum[1])
-				fprintf(stderr, "[ERR] file and buffer checksum don't match!\n");
+				fprintf(stderr, "[ERR] file and buffer checksums don't match!\n");
 			checksum::print(sum[0], argv[i]);
 		}
 	}
