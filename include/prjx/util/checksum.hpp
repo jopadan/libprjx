be8ff755c7db8982a6156eecdd4b47bda68dd7bc
Jon Daniel <joneqdaniel@gmail.com>
2025-03-21 01:03:50 +0100

    update checksum

--- include/prjx/util/checksum.hpp
+++ include/prjx/util/checksum.hpp
@@ -6,33 +6,19 @@
 
 namespace prjx::checksum
 {
-	using namespace prjx::math;
+    using namespace prjx::math;
 
-	inline constexpr u64 read_bytes(u8*& buf, u64& cnt)
-	{
-		u64 ret = -1;
-		/* extract 3 bytes */
-		if(cnt > 2)
-		{
-			ret = buf[2] << 16 | buf[1] << 8 | buf[0] << 0; buf+=3; cnt-=3;
-		}
-		/* extract remaining tail */
-		else if(cnt > 0)
-		{
-			ret = cnt == 2 ? buf[1] << 8 : 0; ret |= buf[0]; buf+=cnt; cnt=0;
-		}
-		return ret;
-	}
-	constexpr i64 compute_block( u8 buf[4096], u64 len )
-	{
-		i64 ret = 0;
-
-		for(u32 shl = 0; len > 0; shl+=7, shl&=0x1F)
-			ret += read_bytes(buf, len) << shl;
-
-		return ret;
-	}
-
+    constexpr i64 compute_block( u8 buf[4096], u64 len )
+    {
+        i64 ret = 0;
+        u32 shl = 0;
+        for(; len > 2; buf+=3, len-=3, shl+=7, shl&=0x1F)
+            ret += (u64)le32toh(*(u32*)buf & 0x00FFFFFF) << shl;
+        if(len > 0)
+            ret += ((len == 2 ? buf[1] << 8 : 0) | buf[0]) << shl;
+        return ret;  
+    } 
+ 
 	constexpr i64 verify( u8* buf, u64 size )
 	{
 		i64 data = 0;
